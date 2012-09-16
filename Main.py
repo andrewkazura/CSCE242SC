a022f0e9f1395cda7c5ae206a2719a1da47803c4
Andrew <Andrew@10.32.10.234>
2012-09-16 19:35:13 -0400

    HW3

--- Main.py
+++ Main.py
@@ -10,20 +10,30 @@ import os
 
 
 jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)+"/templates"))
+from google.appengine.api import users
 
 class MainPage(webapp2.RequestHandler):
+      
       def get(self):
         template_values = {
+                           'user': users.get_current_user(),
                            'url': self.request.get('url'),
-                           'caption': self.request.get('caption')
+                           'caption': self.request.get('caption')  
         }
+        
+        user = users.get_current_user()
+        if user:
+            greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
+                        (user.nickname(), users.create_logout_url("/")))
+        else:
+            greeting = ("<a href=\"%s\">Sign in or register</a>." %
+                        users.create_login_url("/"))
 
+        self.response.out.write("<html><header>%s</header></html>" % greeting)
+  
         template = jinja_environment.get_template('master.html')
         self.response.out.write(template.render(template_values))
-       
-       
-
 
 
 app = webapp2.WSGIApplication([('/', MainPage),
---
+++ README.txt
@@ -0,0 +1,13 @@
+Andrew Kazura
+CSCE 242
+Homework 3
+
+I'm not sure if you said that we needed this so here it is.
+
+The Main page checks to see if the user is logged in and if they are then put their
+userid in the header and a logout button next to it. If there is no user is logged
+in then put a login button in the header.
+
+The master page checks to see if there is data inputted into the Caption and URL 
+variables. If there is something then display it underneath the title otherwise
+display nothing.
